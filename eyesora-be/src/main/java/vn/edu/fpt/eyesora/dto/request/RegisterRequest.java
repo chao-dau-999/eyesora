eb25b0a0ac3c4a604c511c7fd03ed975cc147abc
QUY <quynn.ce190984@gmail.com>
2026-06-14 18:35:26 +0700

    feat: Thêm xác nhận mật khẩu (confirm password) cho API Đăng ký

--- eyesora-be/src/main/java/vn/edu/fpt/eyesora/dto/request/RegisterRequest.java
+++ eyesora-be/src/main/java/vn/edu/fpt/eyesora/dto/request/RegisterRequest.java
@@ -3,6 +3,7 @@ package vn.edu.fpt.eyesora.dto.request;
 public record RegisterRequest(
         String username,
         String password,
+        String confirmPassword,
         String fullName,
         String facilityId
 ) {
