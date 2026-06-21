141cc745cb29a39695e76377b766c6a350370189
chao-dau-999 <NhutLM.CE190737@gmail.com>
2026-06-21 15:13:30 +0700

    refactor(api): update token response

--- eyesora-be/src/main/java/vn/edu/fpt/eyesora/entity/User.java
+++ eyesora-be/src/main/java/vn/edu/fpt/eyesora/entity/User.java
@@ -51,7 +51,7 @@ public class User implements UserDetails {
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         return roles.stream()
-                .map(role -> new SimpleGrantedAuthority(role.getName()))
+                .map(role -> new SimpleGrantedAuthority("ROLE_" + role.getName()))
                 .toList();
     }
 
