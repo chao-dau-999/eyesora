29449577b669a7513570d1148d6f30e24e6293ca
QUY <quynn.ce190984@gmail.com>
2026-06-22 19:43:06 +0700

    Add @Repository annotations

--- eyesora-be/src/main/java/vn/edu/fpt/eyesora/repository/DistrictRepository.java
+++ eyesora-be/src/main/java/vn/edu/fpt/eyesora/repository/DistrictRepository.java
@@ -1,8 +1,10 @@
 package vn.edu.fpt.eyesora.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import vn.edu.fpt.eyesora.entity.District;
 
+@Repository
 public interface DistrictRepository extends JpaRepository<District, String> {
 
 }
--- eyesora-be/src/main/java/vn/edu/fpt/eyesora/repository/WardRepository.java
+++ eyesora-be/src/main/java/vn/edu/fpt/eyesora/repository/WardRepository.java
@@ -3,10 +3,12 @@ package vn.edu.fpt.eyesora.repository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import vn.edu.fpt.eyesora.entity.Ward;
 
 import java.util.List;
 
+@Repository
 public interface WardRepository extends JpaRepository<Ward, String> {
     Page<Ward> findByDistrictId(String districtId, Pageable pageable);
 }
